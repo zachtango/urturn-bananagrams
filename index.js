7b693f20a326357eda5e460b9f4ee6c1e10bbb26
zachtango <38231181+zachtango@users.noreply.github.com>
2022-11-27 07:18:39 +0000

    Deploying to published from @ zachtango/urturn-bananagrams@35376b6c7ebcdc4f434016f5da39ae72dd563184 🚀

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var wordlist = require('wordlist-english');
+var fs = require('fs');
+var path = require('path');
 
 const Status = Object.freeze({
   PreGame: 'preGame',
@@ -86,6 +87,25 @@ const LETTERS = [
   'y', 'y', 'y', 'y',
   'z', 'z', 'z', 'z'
 ];
+
+var expose = {__dirname};
+
+var {__dirname: __dirname$1} = expose; // eslint-disable-line no-shadow
+
+var wordlist = {};
+
+['english', 'american', 'australian', 'british', 'canadian'].forEach(function (dialect) {
+  var dialectKey = dialect === 'english' ? dialect : 'english/' + dialect;
+  var dialectWords = [];
+  [10, 20, 35, 40, 50, 55, 60, 70].forEach(function (frequency) {
+    var frequencyKey = dialectKey + '/' + frequency;
+    var filePath = path.join(__dirname$1, dialect + '-words-' + frequency + '.json');
+    var frequencyWords = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    wordlist[frequencyKey] = frequencyWords;
+    dialectWords.push(...frequencyWords);
+  });
+  wordlist[dialectKey] = dialectWords.sort();
+});
 
 const words = new Set(wordlist['english']);
 
